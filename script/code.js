e1b64ba60015add305cb415730a972081045ecdf
DanielUmetor <dnumetor@gmail.com>
2024-05-16 08:38:25 +0200

    JavaScriptForm

---
+++ script/code.js
@@ -0,0 +1,11 @@
+let friends = []
+let addFriends = document.querySelector ('[addFriends]')
+addFriends.addEventListener('click', () => {
+    friends.push(
+        {
+            firstName: document.querySelector('[firstName]').value,
+            lastName: document.querySelector('[lastName]').value
+        }
+    )
+    console.log(friends);
+})
